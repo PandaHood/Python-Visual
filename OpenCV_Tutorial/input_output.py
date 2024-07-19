419b610a887ab1b202b8386b8f3f3f9cf428ed3f
PandaHood <niksanderson0@gmail.com>
2024-07-18 20:32:51 -0400

    dad

--- OpenCV_Tutorial/input_output.py
+++ OpenCV_Tutorial/input_output.py
@@ -18,6 +18,13 @@ img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
 cv2.imshow('image', img)
 cv2.waitKey(0)
 
+img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+
+# visualize image
+cv2.imshow('image', img)
+cv2.waitKey(0)
+
+print(img.shape)
 
 #write image
 cv2.imwrite("OpenCV_Tutorial\\bird_out.jpg",img)
